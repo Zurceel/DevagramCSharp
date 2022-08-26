8611d54c45fb204ba36bf26f61cf12fde55917aa
Zurceel <gabrielcruzsil@gmail.com>
2022-08-25 21:24:11 -0300

    Inserindo a chave JWT no login

--- DevagramCSharp/Controllers/LoginController.cs
+++ DevagramCSharp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
+using DevagramCSharp.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,7 +24,44 @@ namespace DevagramCSharp.Controllers
 		{
 			try
 			{
-				throw new ArgumentException("Erro ao preencher os dados"); 
+				if(!String.IsNullOrEmpty(loginrequisicao.Senha) && !String.IsNullOrEmpty(loginrequisicao.Email) &&
+					!String.IsNullOrWhiteSpace(loginrequisicao.Senha) && !String.IsNullOrWhiteSpace(loginrequisicao.Senha))
+				{
+					string email = "gabriel@devaria.com.br";
+					string senha = "Grampola123";
+
+					if(loginrequisicao.Email == email && loginrequisicao.Senha == senha)
+					{
+						Usuario usuario = new Usuario()
+						{
+							Email = loginrequisicao.Email,
+							Id = 12,
+							Nome = "Gabriel da Cruz"
+						};
+
+						return Ok(new LoginRespostaDto()
+						{
+							Email = usuario.Email,
+							Nome = usuario.Nome,
+							Token = TokenService.CriarToken(usuario)
+						});
+					}else
+					{
+						return BadRequest(new ErrorRespostaDto()
+						{
+							Descricao = "Email ou senha inválidos!",
+							Status = StatusCodes.Status400BadRequest
+						});
+					}
+				}
+				else
+				{
+					return BadRequest(new ErrorRespostaDto()
+					{
+						Descricao = "Usuário não preencheu os campos de login corretamente",
+						Status = StatusCodes.Status400BadRequest
+					});
+				}
 			}
 			catch (Exception ex)
 			{
---
+++ DevagramCSharp/Dtos/LoginRespostaDto.cs
@@ -0,0 +1,13 @@
+﻿using DevagramCSharp.Models;
+
+namespace DevagramCSharp.Dtos
+{
+	public class LoginRespostaDto
+	{
+		internal Usuario usuario;
+
+		public string Nome { get; set; }
+		public string Email { get; set; }
+		public string Token { get; set; }
+	}
+}
