19c82c3f29fffabd9af116595881477714b48fbe
Zurceel <gabrielcruzsil@gmail.com>
2022-08-31 11:06:50 -0300

    Implementando a atualizacao de usuario.

--- DevagramCSharp/Repository/IUsuarioRepository.cs
+++ DevagramCSharp/Repository/IUsuarioRepository.cs
@@ -8,6 +8,8 @@ namespace DevagramCSharp.Repository
         Usuario GetUsuarioPorLoginSenha(string email, string senha);
 		public void Salvar(Usuario usuario);
 
-        public bool VerificarEmail(string email); 
+        public bool VerificarEmail(string email);
+
+        public void AtualizarUsuario(Usuario usuario);
     }
 }
